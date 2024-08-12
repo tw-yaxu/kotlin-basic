2a25b4bc618321c2aab9968dca6efc28d289b418
ya xu <yaxu@thoughtworks.com>
2024-08-12 18:40:45 +0800

    update test scenario and handle number to char logic

--- src/main/kotlin/com/thoughtworks/kotlin_basic/util/ColumnIndicesUtil.kt
+++ src/main/kotlin/com/thoughtworks/kotlin_basic/util/ColumnIndicesUtil.kt
@@ -1,11 +1,27 @@
 package com.thoughtworks.kotlin_basic.util
 
+import java.lang.StringBuilder
+
 // 1 -> A, 2 -> B, 26 -> Z
 // 27 -> AA, 28 -> AB, ..., 52 -> AZ
 // 53 -> BA, 54 -> BB, ..., 78 -> BZ
+// 26^1 * 2 + 26^0 * 1 = 52 + 1 = 53
 
 class ColumnIndicesUtil {
-    fun getColumnLabelByIndex(index: Int): String {
-        return "A"
+    private fun getColumnLabelByIndex(columnNumber: Int): String {
+        val result = StringBuilder()
+        var temp = columnNumber
+
+        while (temp > 0) {
+            temp --
+            val remainder = temp % 26
+            result.append('A' + remainder)
+            temp /= 26
+        }
+        return result.reverse().toString()
+    }
+
+    fun getColumnLabelsFromConfig(startIndex: Int, count: Int): List<String> {
+        return (startIndex..< startIndex  + count).map { getColumnLabelByIndex(it) }
     }
 }
--- src/test/kotlin/com/thoughtworks/kotlin_basic/util/ColumnIndicesUtilTest.kt
+++ src/test/kotlin/com/thoughtworks/kotlin_basic/util/ColumnIndicesUtilTest.kt
@@ -1,11 +1,22 @@
 package com.thoughtworks.kotlin_basic.util
 
 import kotlin.test.Test
+import kotlin.test.assertEquals
 
 class ColumnIndicesUtilTest {
+    private val columnIndicesUtil = ColumnIndicesUtil()
+
     @Test
-    fun `1 to A`() {
-        val columnIndicesUtil = ColumnIndicesUtil()
-        assert(columnIndicesUtil.getColumnLabelByIndex(1) == "A")
+    fun `1 and 1 to A`() {
+        assertEquals(listOf("A"), columnIndicesUtil.getColumnLabelsFromConfig(1, 1))
+    }
+
+    @Test
+    fun `1 and 2 to A, B`() {
+        assertEquals(listOf("A", "B"), columnIndicesUtil.getColumnLabelsFromConfig(1, 2))
+    }
+    @Test
+    fun `26 and 3 to Z, AA, AB`() {
+        assertEquals(listOf("Z", "AA", "AB"), columnIndicesUtil.getColumnLabelsFromConfig(26, 3))
     }
 }
