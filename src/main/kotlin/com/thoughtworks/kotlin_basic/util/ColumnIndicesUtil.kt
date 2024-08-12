615a1e4b8f5dcbcbd1b7ffea4a6091292cecfb54
ya xu <yaxu@thoughtworks.com>
2024-08-12 18:52:33 +0800

    throw error when larger than ZZZ

--- src/main/kotlin/com/thoughtworks/kotlin_basic/util/ColumnIndicesUtil.kt
+++ src/main/kotlin/com/thoughtworks/kotlin_basic/util/ColumnIndicesUtil.kt
@@ -6,17 +6,27 @@ import java.lang.StringBuilder
 // 27 -> AA, 28 -> AB, ..., 52 -> AZ
 // 53 -> BA, 54 -> BB, ..., 78 -> BZ
 // 26^1 * 2 + 26^0 * 1 = 52 + 1 = 53
+// 26^2 * 26 + 26^1 * 26 + 26^0 * 26 =
 
 class ColumnIndicesUtil {
+    companion object {
+        const val ALPHABET_COUNT = 26
+        const val LARGEST_COLUMN_INDEX = (26 * 26) * 26 + (26) * 26 + 26
+    }
+
     private fun getColumnLabelByIndex(columnNumber: Int): String {
+        if (columnNumber > LARGEST_COLUMN_INDEX) {
+            throw IllegalArgumentException("Label should be smaller than ZZZ")
+        }
+
         val result = StringBuilder()
         var temp = columnNumber
 
         while (temp > 0) {
             temp --
-            val remainder = temp % 26
+            val remainder = temp % ALPHABET_COUNT
             result.append('A' + remainder)
-            temp /= 26
+            temp /= ALPHABET_COUNT
         }
         return result.reverse().toString()
     }
--- src/test/kotlin/com/thoughtworks/kotlin_basic/util/ColumnIndicesUtilTest.kt
+++ src/test/kotlin/com/thoughtworks/kotlin_basic/util/ColumnIndicesUtilTest.kt
@@ -2,6 +2,7 @@ package com.thoughtworks.kotlin_basic.util
 
 import kotlin.test.Test
 import kotlin.test.assertEquals
+import kotlin.test.assertFailsWith
 
 class ColumnIndicesUtilTest {
     private val columnIndicesUtil = ColumnIndicesUtil()
@@ -15,8 +16,22 @@ class ColumnIndicesUtilTest {
     fun `1 and 2 to A, B`() {
         assertEquals(listOf("A", "B"), columnIndicesUtil.getColumnLabelsFromConfig(1, 2))
     }
+
     @Test
     fun `26 and 3 to Z, AA, AB`() {
         assertEquals(listOf("Z", "AA", "AB"), columnIndicesUtil.getColumnLabelsFromConfig(26, 3))
+    }
+
+    @Test
+    fun `18278 to ZZZ`() {
+        assertEquals(listOf("ZZZ"), columnIndicesUtil.getColumnLabelsFromConfig((26 * 26) * 26 + (26) * 26 + 26, 1))
+    }
+
+    @Test
+    fun `throw error when start index is larger than 1378`() {
+        val exception = assertFailsWith<IllegalArgumentException> {
+            columnIndicesUtil.getColumnLabelsFromConfig((26 * 26) * 26 + (26) * 26 + 26 + 1, 1)
+        }
+        assertEquals("Label should be smaller than ZZZ", exception.message)
     }
 }
